4502bb06c20d49401f61680a4f1804bec22a9da3
JNSPK <johan.nilsson.mail@gmail.com>
2022-08-31 16:06:33 +0200

    Adding lorem to have idea of spacing

--- Animations.js
+++ Animations.js
@@ -1,19 +1,17 @@
 var AboutMe = document.querySelector(".aboutme");
 
-document.querySelector(".aboutmepage").addEventListener("click", function () {
+document.querySelector(".aboutme").addEventListener("click", function () {
   AboutMe.classList.toggle("aboutmeopen");
 });
 
 var Achievements = document.querySelector(".achievements");
 
-document
-  .querySelector(".achievementspage")
-  .addEventListener("click", function () {
-    Achievements.classList.toggle("achievementsopen");
-  });
+document.querySelector(".achievements").addEventListener("click", function () {
+  Achievements.classList.toggle("achievementsopen");
+});
 
 var Contact = document.querySelector(".contact");
 
-document.querySelector(".contactpage").addEventListener("click", function () {
+document.querySelector(".contact").addEventListener("click", function () {
   Contact.classList.toggle("contactopen");
 });
